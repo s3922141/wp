a89adffc4c568de68f7e74c190b7ee0a691755cc
s3922141 <90558734+s3922141@users.noreply.github.com>
2023-08-14 20:03:00 +0930

    Added smart nav links
    Added implementation on index that allows links to update the link associated with the currently viewed section.

--- a3/script.js
+++ a3/script.js
@@ -1,1 +1,57 @@
-/* Insert your javascript here */
+alert("JavaScript is working!");
+
+function updateAboutUsLink() {
+    const aboutUsSection = document.getElementById('About-Us');
+    const aboutUsLink = document.querySelector('nav a[href="#About-Us"]');
+
+    const rect = aboutUsSection.getBoundingClientRect();
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+    // Check if the section is in view and takes up at least half of the screen
+    const isAboutUsSectionInView = rect.top <= windowHeight / 2 && rect.bottom >= windowHeight / 2;
+
+    if (isAboutUsSectionInView) {
+        aboutUsLink.classList.add('active');
+    } else {
+        aboutUsLink.classList.remove('active');
+    }
+}
+
+function updateSeatsandPricesLink() {
+    const aboutUsSection = document.getElementById('Seats-and-Prices');
+    const aboutUsLink = document.querySelector('nav a[href="#Seats-and-Prices"]');
+
+    const rect = aboutUsSection.getBoundingClientRect();
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+    // Check if the section is in view and takes up at least half of the screen
+    const isAboutUsSectionInView = rect.top <= windowHeight / 2 && rect.bottom >= windowHeight / 2;
+
+    if (isAboutUsSectionInView) {
+        aboutUsLink.classList.add('active');
+    } else {
+        aboutUsLink.classList.remove('active');
+    }
+}
+
+function updateNowShowingLink() {
+    const aboutUsSection = document.getElementById('Now-Showing');
+    const aboutUsLink = document.querySelector('nav a[href="#Now-Showing"]');
+
+    const rect = aboutUsSection.getBoundingClientRect();
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+    // Check if the section is in view and takes up at least half of the screen
+    const isAboutUsSectionInView = rect.top <= windowHeight / 2 && rect.bottom >= windowHeight / 2;
+
+    if (isAboutUsSectionInView) {
+        aboutUsLink.classList.add('active');
+    } else {
+        aboutUsLink.classList.remove('active');
+    }
+}
+
+// Add scroll event listener to update the active link on scroll
+window.addEventListener('scroll', updateAboutUsLink);
+window.addEventListener('scroll', updateSeatsandPricesLink);
+window.addEventListener('scroll', updateNowShowingLink);
